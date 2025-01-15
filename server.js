c39695613b2fe630c6b490d52cf8602f0a40de52
imsajib02 <shafiul.imsajib@gmail.com>
2025-01-15 12:06:28 +0600

    Covert the message data to json before broadcasting

--- server.js
+++ server.js
@@ -54,9 +54,12 @@ chatWss.on('connection', (ws) => {
 
 // Function to broadcast messages to connected clients
 function broadcastMessage(clients, message) {
+
+    const messageData = JSON.stringify({ message: message });
+
     clients.forEach((client) => {
         if(client.readyState === WebSocket.OPEN) {
-            client.send(message);
+            client.send(messageData);
         }
     });
 }
