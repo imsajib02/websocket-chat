315edc8bebfc32552bbcce135ddfc4f1066a0cb8
imsajib02 <shafiul.imsajib@gmail.com>
2025-01-16 09:21:45 +0600

    Log message updated

--- server.js
+++ server.js
@@ -95,5 +95,5 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
+    console.log(`Server is listening on ${PORT}`);
 });
